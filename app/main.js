c616dafc8d16e91e25aebf7c85c34d79a4dbb5f2
Henrique Dias <hdias8969@gmail.com>
2025-01-28 17:59:27 -0300

    Arquivo para função MAP criado

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 let livros = []
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaApi()
-const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endpointDaApi)
@@ -10,21 +9,3 @@ async function getBuscarLivrosDaApi() {
     exibirOsLivrosNaTela(livros)
 }
 
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += (`
-    <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-            alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-            ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-        </div>
-    </div>
-    `)
-    });
-}
