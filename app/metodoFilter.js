2e5ea709aa64dd0cd0781afa8091660083b52c01
Henrique Dias <hdias8969@gmail.com>
2025-01-29 14:46:38 -0300

    Adicionada verificação para garantir que a propriedade  quantidade existe nos objetos de livro.

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,25 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria === 'disponível' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria === categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel') {
+        exibirValorTotal()
+    }
 } 
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotal () {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `
+}
